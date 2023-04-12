dfb8c4d92662795b940934b1c482943cc8849acb
emanuelevolpes <emanuele.volpes1@gmail.com>
2023-04-12 18:33:37 +0200

    write-on-server

--- main.js
+++ main.js
@@ -2,15 +2,32 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            todos: []
+            todos: [],
+            newToDo: ''
+        }
+    },
+    methods: {
+        addToDo() {
+            const data = {
+                newtodo: this.newToDo
+            };
+
+            axios
+                .post('server.php', data, {
+                    headers: { 'Content-Type': 'multipart/form-data' }
+                })
+                .then((response) => {
+                    console.log(response);
+                    this.todos = response.data;
+                })
         }
     },
     created() {
         axios
-        .get('server.php')
-        .then((response) => {
-            console.log(response);
-            this.todos = response.data;
-        })
+            .get('server.php')
+            .then((response) => {
+                console.log(response);
+                this.todos = response.data;
+            })
     }
 }).mount('#app')
--- server.php
+++ server.php
@@ -1,18 +1,30 @@
 <?php
 
+$newtodo = isset($_POST['newtodo']) ? $_POST['newtodo'] : NULL;
+
 $todo = file_get_contents('todo.json');
 
 // var_dump($todo);
 
 $todo = json_decode($todo, true);
 
+if($newtodo !== NULL){
+    $todo[] = [
+        "text" => $newtodo,
+        "done" => false
+    ];
+};
+
 // var_dump($todo);
 
 $todo = json_encode($todo);
+file_put_contents('todo.json', $todo);
+
 
 // $todo[0]['text'] = 'ciao';
 
 //var_dump($todo);
+header('Content-Type: application/json');
 
 echo $todo;
 
